7d0e1e8ffa0a9e7dc145f4c69d92207af747b519
DawidZapo <dawidzapo@gmail.com>
2024-05-22 14:08:32 +0200

    CORSFilter introduced. Bug with /api/team path resolved

--- src/main/java/com/stat_tracker/rest_controller/team/TeamController.java
+++ src/main/java/com/stat_tracker/rest_controller/team/TeamController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*", maxAge = 3600)
+
 public class TeamController {
     private TeamService teamService;
 
@@ -19,7 +19,7 @@ public class TeamController {
     public TeamController(TeamService teamService) {
         this.teamService = teamService;
     }
-    @GetMapping("/teams")
+    @GetMapping("/team/all")
     public ResponseEntity<List<Team>> getTeams(){
         return ResponseEntity.ok(teamService.findAll());
     }
